3a19266bba415de3253a025e78964b78d4ec2555
Philipp Müller <heiligster@googlemail.com>
2016-04-12 23:09:01 +0200

    Implemented ability to delete gists

--- index.js
+++ index.js
@@ -6,44 +6,40 @@ const fs = require('fs')
 const program = require('commander')
 const request = require('superagent')
 const chalk = require('chalk')
-const config = require('./config')
+const cfg = require('./config')
 const version = '0.0.1'
 
 /**
- * Handle response from api calls
+ * Prepare the output string
  */
-const handleResponse = (err, res) => {
-  if (err) {
-    console.log(chalk.red(err))
-    return
-  }
+const prepareOutput = (current, index) => {
+  let output = '----------------------------\n'
 
-  console.log(chalk.green('Gist created: ') + res.body.html_url)
-}
+  output += 'ID: ' + current.id + '\n'
+  output += 'URL: ' + current.url + '\n'
+  output += 'Description: ' + current.description + '\n'
+  output += 'Public: ' + current.public + '\n'
+  output += '----------------------------\n'
 
-/**
- * Parse response from api call
- */
-const parseResponse = (res) => {
-  res.forEach(function (gist) {
-    console.log(gist.url)
-  })
+  console.log(output)
 }
 
 program.version(version)
+program.usage('[command] [options]')
 
 program
   .command('list')
-  .description('Get a list of gists')
-  .option(config.username.option, config.username.description)
-  .option(config.password.option, config.password.description)
+  .description(cfg.commands.list)
+  .option(cfg.username.option, cfg.username.description)
+  .option(cfg.password.option, cfg.password.description)
   .action((options) => {
     if (!options.username) {
-      console.log(chalk.red('Please provide at least your username'))
+      console.log(chalk.red(cfg.messages.username))
+
       return
     }
 
-    let req = request.get(config.endpoints.get + options.username + '/gists')
+    let req = request.get(cfg.endpoints.get + options.username + '/gists')
 
     if (options.password) {
       req.auth(options.username, options.password)
@@ -52,28 +48,31 @@ program
     req.end((err, res) => {
       if (err) {
         console.error(chalk.red(err.status, err.message))
+
         return
       }
 
-      parseResponse(res.body)
+      res.body.forEach(prepareOutput)
     })
   })
 
 program
   .command('create <file>')
-  .description('Create a new gist')
-  .option(config.username.option, config.username.description)
-  .option(config.password.option, config.password.description)
-  .option(config.info.option, config.info.description)
-  .option(config.public.option, config.public.description)
+  .description(cfg.commands.create)
+  .option(cfg.username.option, cfg.username.description)
+  .option(cfg.password.option, cfg.password.description)
+  .option(cfg.info.option, cfg.info.description)
+  .option(cfg.public.option, cfg.public.description)
   .action((file, options) => {
     if (!options.username) {
-      console.log(chalk.red('Please provide a username'))
+      console.log(chalk.red(cfg.messages.username))
+
       return
     }
 
     if (!options.password) {
-      console.log(chalk.red('Please provide a password'))
+      console.log(chalk.red(cfg.messages.password))
+
       return
     }
 
@@ -86,18 +85,52 @@ program
     fs.readFile(file, 'utf8', function (err, data) {
       if (err) {
         console.error('Oh no! ', chalk.red(err))
+
         return
       }
 
       payload.files[file] = { 'content': data }
 
       request
-        .post(config.endpoints.post)
+        .post(cfg.endpoints.post)
         .auth(options.username, options.password)
         .set('Content-Type', 'application/json')
         .send(JSON.stringify(payload))
-        .end(handleResponse)
+        .end((err, res) => {
+          if (err) {
+            console.log(chalk.red(err))
+
+            return
+          }
+
+          console.log(chalk.green('Gist created: ') + res.body.html_url)
+        })
     })
+  })
+
+program
+  .command('delete <id>')
+  .description(cfg.commands.delete)
+  .option(cfg.username.option, cfg.username.description)
+  .option(cfg.password.option, cfg.password.description)
+  .action((id, options) => {
+    if (!options.username || !options.password) {
+      console.error(chalk.red(cfg.messages.required))
+
+      return
+    }
+
+    request
+      .del(cfg.endpoints.delete + id)
+      .auth(options.username, options.password)
+      .end((err, res) => {
+        if (err) {
+          console.error(chalk.red(err.status, err.message))
+
+          return
+        }
+        // 204 No content means all is fine
+      })
   })
 
 program.parse(process.argv)
